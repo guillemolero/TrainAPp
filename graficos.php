5e456fc1148e02f2a9b52ea3f028d363c149a8aa
José Francisco Esparza <jesparza@aqubix.com>
2017-05-19 20:47:28 +0200

    Nuevos gráficos. Todavía sin personalizar

---
+++ graficos.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Gráficos</title>
+        <link rel="stylesheet" href="css/graficos.css">
+    </head>
+    <body>
+        <div id ="npeso">
+            Peso levantado en kgs
+        </div>
+<div class="chart-container">
+  <div class="y-value value-100">100</div>
+  <div class="y-value value-90">90</div>
+  <div class="y-value value-80">80</div>
+  <div class="y-value value-70">70</div>
+  <div class="y-value value-60">60</div>
+  <div class="y-value value-50">50</div>
+  <div class="y-value value-40">40</div>
+  <div class="y-value value-30">30</div>
+  <div class="y-value value-20">20</div>
+  <div class="y-value value-10">10</div>
+  <div class="y-value value-0">0</div>
+  <div class="chart">
+    <div class="bar blue" style="height: calc(28em*0.32)"></div>
+    <div class="bar purple" style="height: calc(28em*0.56)"></div>
+    <div class="bar green" style="height: calc(28em*0.12)"></div>
+  </div>
+</div>
+        <h2 class="info">Registro de <span id="ejercicio">ejercicio</span> de <span id="usuario">usuario</span></h2>
+    </body>
+</html>
