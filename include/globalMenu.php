5b6331ee30ca6a7fd6b6791e54b599c59d0f93ca
Guille <Guille@PC-Guille>
2017-06-07 18:31:22 +0200

    REESTRUCTURACION

--- include/globalMenu.php
+++
@@ -1,8 +0,0 @@
-<nav>
-    <ul>
-        <li><a href="calendar.php" class="icon fa-calendar"><span class="label">Calendario</span></a></li>
-        <li><a href="graphics.php" class="icon fa-area-chart"><span class="label">Gráficos</span></a></li>
-        <li><a href="#" class="icon fa-line-chart"><span class="label">Estadísticas</span></a></li>
-        <li><a href="functions/logout.php" class="icon fa-power-off"><span class="label">Cerrar sesión</span></a></li>
-    </ul>
-</nav>
